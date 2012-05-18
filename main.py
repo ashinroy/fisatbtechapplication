bcf1c620a65aa49d1dcc7ed4af2d63e153ae37e9
mahesh.fisat <mahesh.fisat@gmail.com>
2012-05-18 16:49:07 +0000

    basic featues done , fixed floating point errors

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ class MainPage(webapp.RequestHandler):
 		path = os.path.join(os.path.dirname(__file__), 'apptemplate.html')
 		self.response.out.write(template.render(path, values))
 	def validate_name(self,name):
-		USER_RE = re.compile("^[a-zA-Z][a-zA-Z ]+$")
+		USER_RE = re.compile("^[a-zA-Z. ][a-zA-Z. ]+$")
 		return USER_RE.match(name)	
 	def validate_phno(self,phone):
 		RE = re.compile("^[0-9-]+$")
@@ -50,13 +50,13 @@ class MainPage(webapp.RequestHandler):
 		EMAIL_RE = re.compile("^[\S]+@[\S]+\.[\S]+$")
 		return EMAIL_RE.match(email)
 	def validate_erollno(self,rollno):
-		RE = re.compile("^[0-9]+$")
+		RE = re.compile("^[0-9A-Za-z-/]+$")
 		return (RE.match(rollno)and(len(rollno)>erollno_length))
 	def validate_erank(self,rank):
 		RE = re.compile("^[0-9]+$")
 		return (RE.match(rank))
 	def validate_mark(self,mark):
-		RE = re.compile("^[0-9-]+$")
+		RE = re.compile("^[0-9-.]+$")
 		return (RE.match(mark))
 	def validate_year(self,year):
 		RE = re.compile("^[0-9-]+$")
@@ -861,7 +861,341 @@ class PrintApp(webapp.RequestHandler):
 		doc.build(App)
 
 class ReprintApp(webapp.RequestHandler):
-	pass
+	def chopline(self,line):
+		if len(line)<=chop_length:
+			return line
+		try:		
+			cant = len(line) /chop_length
+			cant += 1
+			strline = ""
+			index = chop_length
+			for i in range(1,cant):
+				index = chop_length * i
+				strline += "%s\n" %(line[(index-chop_length):index])
+			strline += "%s\n" %(line[index:])
+			return strline
+		except:
+			return line
+	def check_id(self,erollno):
+		btechapp=btechApp.all()
+		btechapp.filter("erollno =",erollno)
+		try:		
+			app=btechapp.fetch(1)[0]
+			return True
+		except:
+			return False
+	def get(self):
+		reprint={"error":""}
+		values={"reprint":reprint}
+		path = os.path.join(os.path.dirname(__file__), 'reprint.html')
+		self.response.out.write(template.render(path, values))
+		
+	def post(self):
+		reprint={"error":""}
+		values={"reprint":reprint}
+		appid=self.request.get("appid")
+		print appid[5:]
+		if self.check_id(appid[4:]):
+			self.print_pdf(appid)
+			pass
+		else:
+			reprint["error"]="Application ID does not exist. Please contact college"
+			path = os.path.join(os.path.dirname(__file__), 'reprint.html')
+			self.response.out.write(template.render(path, values))
+		
+		
+	def print_pdf(self,appid):
+		
+		btechapp=btechApp.all()
+		btechapp.filter("appid =",appid)
+		app=btechapp.fetch(1)[0]
+		self.response.headers['Content-Type'] = 'application/pdf'
+		self.response.headers['Content-Disposition'] = 'attachment;filename=%s.pdf' % appid
+		doc = SimpleDocTemplate(self.response.out,pagesize=A4,rightMargin=20,leftMargin=20,topMargin=30,bottomMargin=30)
+		styles=getSampleStyleSheet()
+		styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))		
+		styles.add(ParagraphStyle(name='Left', alignment=TA_LEFT))
+		styles.add(ParagraphStyle(name='Right', alignment=TA_RIGHT))
+		styles.add(ParagraphStyle(name='Center', alignment=TA_CENTER))	
+		
+
+		nametext=Paragraph("<para fontSize=10>Name:</para>",styles["Left"])
+		name=Paragraph("<para fontSize=10><b>%s</b></para>" % app.name,styles["Left"])
+		paddresstext=Paragraph("<para fontSize=10>Permanent Address:</para>",styles["Justify"])
+		caddress=Paragraph("<para fontSize=10><b>%s</b></para>" % self.chopline(app.caddress),styles["Left"])
+		caddresstext=Paragraph("<para fontSize=10>Communication Address:</para>",styles["Left"])
+		paddress=Paragraph("<para fontSize=10><b>%s</b></para>" % self.chopline(app.paddress),styles["Justify"])
+		dobtext=Paragraph("<para fontSize=10>Date of Birth:</para>",styles["Left"])		
+		dob=Paragraph("<para fontSize=10><b>%s</b></para>" % app.dob,styles["Left"])
+		emailtext=Paragraph("<para fontSize=10>Email:</para>",styles["Left"])		
+		email=Paragraph("<para fontSize=10><b>%s</b></para>" % app.email,styles["Left"])
+		resphonetext=Paragraph("<para fontSize=10>Residential Phone:</para>",styles["Left"])		
+		resphone=Paragraph("<para fontSize=10><b>%s</b></para>" % app.resphone,styles["Left"])
+		mobphonetext=Paragraph("<para fontSize=10>Mobile Phone:</para>",styles["Left"])		
+		mobphone=Paragraph("<para fontSize=10><b>%s</b></para>" % app.mobphone,styles["Left"])
+		gendertext=Paragraph("<para fontSize=10>Gender:</para>",styles["Left"])		
+		gender=Paragraph("<para fontSize=10><b>%s</b></para>" % app.gender,styles["Left"])
+		nationtext=Paragraph("<para fontSize=10>Nationality:</para>",styles["Left"])		
+		panchayathtext=Paragraph("<para fontSize=10>Panchayath:</para>",styles["Left"])
+		panchayath=Paragraph("<para fontSize=10><b>%s</b></para>" % app.panchayath,styles["Left"])
+		nation=Paragraph("<para fontSize=10><b>%s</b></para>" % app.nation,styles["Left"])
+		religiontext=Paragraph("<para fontSize=10>Religion:</para>",styles["Left"])		
+		religion=Paragraph("<para fontSize=10><b>%s</b></para>" % app.religion,styles["Left"])
+		castetext=Paragraph("<para fontSize=10>Community:</para>",styles["Left"])
+		caste=Paragraph("<para fontSize=10><b>%s</b></para>" % app.caste,styles["Left"])
+		categorytext=Paragraph("<para fontSize=10>Category:</para>",styles["Left"])
+		category=Paragraph("<para fontSize=10><b>%s</b></para>" % app.category,styles["Left"])
+		fathernametext=Paragraph("<para fontSize=10>Father's Name:</para>",styles["Left"])
+		fathername=Paragraph("<para fontSize=10><b>%s</b></para>" % self.chopline(app.fathername),styles["Left"])
+		fatherocctext=Paragraph("<para fontSize=10>Occupation:</para>",styles["Left"])
+		fatherocc=Paragraph("<para fontSize=10><b>%s</b></para>" % self.chopline(app.fatherocc),styles["Left"])
+		fatherdesigtext=Paragraph("<para fontSize=10>Designation:</para>",styles["Left"])
+		fatherdesig=Paragraph("<para fontSize=10><b>%s</b></para>" % self.chopline(app.fatherdesig),styles["Left"])
+		fatheraddress=Paragraph("<para fontSize=10>Address:</para>",styles["Left"])
+		fatherphonetext=Paragraph("<para fontSize=10>Phone:</para>",styles["Left"])
+		fatherphone=Paragraph("<para fontSize=10><b>%s</b></para>" % app.fatherphone,styles["Left"])
+		fatheraddresstext=Paragraph("<para fontSize=10>Address:</para>",styles["Left"])
+		fatheraddress=Paragraph("<para fontSize=10><b>%s</b></para>" % self.chopline(app.fatheraddress),styles["Left"])
+		mothernametext=Paragraph("<para fontSize=10>Mother's Name:</para>",styles["Left"])
+		mothername=Paragraph("<para fontSize=10><b>%s</b></para>" % self.chopline(app.mothername),styles["Left"])
+		motherocctext=Paragraph("<para fontSize=10>Occupation:</para>",styles["Left"])
+		motherocc=Paragraph("<para fontSize=10><b>%s</b></para>" % self.chopline(app.motherocc),styles["Left"])
+		motherdesigtext=Paragraph("<para fontSize=10>Designation:</para>",styles["Left"])
+		motherdesig=Paragraph("<para fontSize=10><b>%s</b></para>" % self.chopline(app.motherdesig),styles["Left"])
+		motheraddresstext=Paragraph("<para fontSize=10>Address:</para>",styles["Left"])
+		motheraddress=Paragraph("<para fontSize=10><b>%s</b></para>" % self.chopline(app.motheraddress),styles["Left"])
+		motherphonetext=Paragraph("<para fontSize=10>Phone:</para>",styles["Left"])
+		motherphone=Paragraph("<para fontSize=10><b>%s</b></para>" % app.motherphone,styles["Left"])
+		
+
+		mfboaesmemnotext=Paragraph("<para fontSize=10>FBOAES(Member) Membership No:</para>",styles["Left"])
+		mfboaesmemno=Paragraph("<para fontSize=10><br/><b>%s</b></para>" % app.mfboaesmemno,styles["Left"])
+		pfboaesmemnotext=Paragraph("<para fontSize=10>FBOAES(Patron) Membership No:</para>",styles["Left"])
+		pfboaesmemno=Paragraph("<para fontSize=10><b>%s</b></para>" % app.pfboaesmemno,styles["Left"])
+		incometext=Paragraph("<para fontSize=10>Annual Income:</para>",styles["Left"])
+		income=Paragraph("<para fontSize=10><b>%s</b></para>" % app.income,styles["Left"])
+		eexamtext=Paragraph("<para fontSize=12><b>Kerala Entrance 2012 </b></para>",styles["Left"])
+		erollnotext=Paragraph("<para fontSize=10>Roll No:</para>",styles["Left"])
+		erollno=Paragraph("<para fontSize=10><b>%s</b></para>" % app.erollno,styles["Left"])
+		eranktext=Paragraph("<para fontSize=10>Rank No:</para>",styles["Left"])
+		#print app.entrank
+		erank=Paragraph("<para fontSize=10><b>%s</b></para>" % app.erank,styles["Left"])
+		epcmarks=Paragraph("<para fontSize=10>Physics and Chemistry<br/>Mark:<b>%s</b>&nbsp;&nbsp;Max:<b>%s</b></para>" % (app.epcmark,app.epcmaxmark),styles["Left"])
+		emmarks=Paragraph("<para fontSize=10>Maths<br/>Mark:<b>%s</b>&nbsp;&nbsp;Max:<b>%s</b></para>" % (app.emmark,app.emmaxmark),styles["Left"])
+		qualexamdtltext=Paragraph("<para fontSize=12><b>Qualifying Exam</b></para>",styles["Left"])
+		qualexamno=Paragraph("<para fontSize=10>Roll No:<b>%s</b></para>"% app.qualexamno,styles["Left"])
+		qualexamboardyear=Paragraph("<para fontSize=10>Year:<b>%s</b><br/>Board:<b>%s</b></para>" % (app.qualexamyear,app.qualboard),styles["Left"])
+		qualexamtext=Paragraph("<para fontSize=10>Qualifying exam:</para>",styles["Left"])		
+		qualexam=Paragraph("<para fontSize=10><b>%s</b></para>" % app.qualexam,styles["Left"])
+		qpmarks=Paragraph("<para fontSize=10>Physics<br/>Mark:<b>%s</b>&nbsp;&nbsp;Max:<b>%s</b></para>" % (app.qpmark,app.qpmaxmark),styles["Left"])
+		qcmarks=Paragraph("<para fontSize=10>Chemistry<br/> Mark:<b>%s</b>&nbsp;&nbsp;Max:<b>%s</b></para>" % (app.qcmark,app.qcmaxmark),styles["Left"])
+		qmmarks=Paragraph("<para fontSize=10>Maths<br/>Mark:<b>%s</b>&nbsp;&nbsp;Max:<b>%s</b></para>" % (app.qmmark,app.qmmaxmark),styles["Left"])
+		
+		
+		choicetitle=Paragraph("<para fontSize=12><b>Branch preferences</b></para>",styles["Left"])
+		choice1=Paragraph("<para fontSize=10>Choice1:<b>%s</b></para>" % (app.bp1),styles["Left"])
+		choice2=Paragraph("<para fontSize=10>Choice2:<b>%s</b></para>" % (app.bp2),styles["Left"])
+		choice3=Paragraph("<para fontSize=10>Choice3:<b>%s</b></para>" % (app.bp3),styles["Left"])
+		choice4=Paragraph("<para fontSize=10>Choice4:<b>%s</b></para>" % (app.bp4),styles["Left"])
+		choice5=Paragraph("<para fontSize=10>Choice5:<b>%s</b></para>" % (app.bp5),styles["Left"])
+		choice6=Paragraph("<para fontSize=10>Choice6:<b>%s</b></para>" % (app.bp6),styles["Left"])
+		insttext=Paragraph("<para fontSize=10><b>Name and address of the school/institution last studied:</b><br/>%s<para>" %  (app.insaddress),styles["Left"])
+		insphone=Paragraph("<para fontSize=10><b>Phone:</b><br/>%s</para>" %  (app.insphone),styles["Left"])
+		extratext=Paragraph("<para fontSize=10><b>Extra-curricular activities:</b><br/>%s<para>" %  (app.extra),styles["Left"])
+		addinfo=Paragraph("<para fontSize=10><b>Additional Information:</b><br/>%s</para>" %  (app.addinfo),styles["Left"])		
+		payinfo=Paragraph("<para fontSize=12><b>Payment Information</b></para>",styles["Left"])		
+		ddno=Paragraph("<para fontSize=10><b>DD No:</b><br/>%s<para>" %  (app.ddno),styles["Left"])
+		dddate=Paragraph("<para fontSize=10><b>DD Date:</b><br/>%s<para>" %  (app.dddate),styles["Left"])
+		ddbank=Paragraph("<para fontSize=10><b>Bank:</b><br/>%s<para>" %  (app.ddbank),styles["Left"])
+		ddbranch=Paragraph("<para fontSize=10><b>Branch:</b><br/>%s<para>" %  (app.ddbranch),styles["Left"])
+		
+		applicantdec=Paragraph("<para fontSize=10><b>Declaration</b><br/><br/>I hereby solemnly affirm that the statement made and information furnished in my application and also in all the enclosures there to submitted by me are true. I declare that, I shall, if admitted, abide by the rules and regulations of the college. I will not engage in any undesirable activity either inside or outside the College that will adversely affect the orderly working, discipline and the reputation of the college.</b><br/></para>",styles["Justify"])
+		station=Paragraph("<para fontSize=10>Station:</para>",styles["Left"])
+		sign=Paragraph("<para fontSize=10>Signature:</para>",styles["Left"])
+		date=Paragraph("<para fontSize=10>Date:</para>",styles["Left"])
+		appname=Paragraph("<para fontSize=10>Name:%s</para>" % app.name	,styles["Left"])
+
+		parentdec=Paragraph("<para fontSize=10> If my son/daughter/ward <b>%s</b> is admitted to the College,I hereby undertake to see to his/her good conduct and discipline within and outside the College.</b><br/></para>"% app.name,styles["Justify"])
+		parentname=Paragraph("<para fontSize=10>Name:</para>" 	,styles["Left"])	
+		officeusetext=Paragraph("<para fontSize=10><b>Office Use</b><br/><br/><br/>Certificate is verified by ............................................<br/><br/><br/><br/>Admitted to Branch ..........on ....................................<br/><br/><br/><br/><br/><br/>Administrative Officer / Superintendent </para>",styles["Left"])
+
+		personifodata=[[paddresstext,paddress,caddresstext,caddress],
+				[dobtext,dob,emailtext,email],
+				[panchayathtext,panchayath,nationtext,nation],
+				[nationtext,nation,religiontext,religion],
+				[castetext,caste,categorytext,category],
+				[mfboaesmemnotext,mfboaesmemno,pfboaesmemnotext,pfboaesmemno],
+				[fathernametext,fathername,mothernametext,mothername],
+				[fatherocctext,fatherocc,motherocctext,motherocc],
+				[fatherdesigtext,fatherdesig,motherdesigtext,motherdesig],
+				[fatheraddresstext,fatheraddress,motheraddresstext,motheraddress],
+				[fatherphonetext,fatherphone,motherphonetext,motherphone],
+				[incometext,income]]
+
+		eexamtabledata=[[eexamtext],
+				[erollnotext,erollno,eranktext,erank],
+				[epcmarks,emmarks]]
+		qexamtabledata=[[qualexamdtltext],
+						[qualexamtext,qualexam],
+						[qualexamboardyear,qualexamno],
+						[qpmarks,qcmarks,qmmarks]]
+
+		choicedata=[[choicetitle],
+					[choice1,choice2,choice3,choice4,choice5,choice6],
+						]
+		
+		extradata=[[insttext,insphone],
+					[extratext,addinfo]]
+		payinfodata=[[payinfo],[ddno,dddate],[ddbank,ddbranch]]
+
+		appdecdata=[[applicantdec],[station,sign],[date,appname]]
+		parentdecdata=[[parentdec],[station,sign],[date,parentname]]
+		
+		appidtext=Paragraph("<para fontSize=12>APPLICATION ID: %s" % appid,styles["Left"])
+		inscopy=Paragraph("<para fontSize=12>INSTITUTE COPY</b></para>",styles["Right"])
+		candcopy=Paragraph("<para fontSize=12>CANDIDATE COPY</b></para>",styles["Right"])
+		
+		
+		institle=Paragraph("<para fontSize=15>FEDERAL INSTITUTE OF SCIENCE AND TECHNOLOGY (FISAT)<font size='10'><super>TM</super></font></para>",styles["Center"])
+		iso=Paragraph("<para fontSize=10>(ISO 9001:2000 Certified Engineering College managed by the Federal Bank Officers' Association Educational Society)</para>",styles["Center"])
+		address=Paragraph("<para fontSize=11><b>HORMIS NAGAR, MOOKKANNOOR P.O., ANGAMALY - 683 577, KERALA</b></para>",styles["Center"])
+		approval=Paragraph("<para fontSize=11>(Approved by AICTE - Affiliated to Mahatma Gandhi University, Kottayam)</para>",styles["Center"])
+		web=Paragraph("<para fontSize=11>Website: www.fisat.ac.in E-mail: mail@fisat.ac.in</para>",styles["Center"])	
+		
+		
+		
+		barcode=code39.Extended39(appid,barWidth=0.5*mm,barHeight=15*mm,humanReadable=True)		
+		photo=Image('photo.jpg',36*mm, 36*mm)
+		Category=Paragraph("<para fontSize=10><b>Category: General</b><br/></para>",styles["Left"])
+		
+		headerinstable=Table([[appidtext,inscopy]])
+		headercantable=Table([[appidtext,candcopy]])
+
+		
+		titletable=Table([[institle],[iso],[address],[approval],[web]])
+		titletable.setStyle(TableStyle([('ALIGN',(0,0),(-1,-1),'CENTER'),
+								('VALIGN',(0,0),(-1,-1),'TOP'),
+								]))
+		linetable=Table([[self.add_space(13),self.add_space(13)]],rowHeights =[5*mm])
+		linetable.setStyle(TableStyle([('ALIGN',(0,0),(-1,-1),'CENTER'),
+								('LINEBELOW',(0,0),(-1,-1),.2*mm,colors.black),
+								]))
+		
+		qexamtable=Table(qexamtabledata)
+		qexamtable.setStyle(TableStyle([('ALIGN',(0,0),(-1,-1),'LEFT'),
+								('VALIGN',(0,0),(-1,-1),'TOP'),]))
+
+		eexamtable=Table(eexamtabledata)
+		eexamtable.setStyle(TableStyle([('ALIGN',(0,0),(-1,-1),'LEFT'),
+								('VALIGN',(0,0),(-1,-1),'TOP'),]))
+
+
+		personifotable=Table(personifodata)
+		personifotable.setStyle(TableStyle([('ALIGN',(0,0),(-1,-1),'LEFT'),
+								('ALIGN',(1,0),(1,0),'LEFT'),
+								('VALIGN',(0,0),(-1,-1),'TOP'),
+								]))
+		choicetable=Table(choicedata)
+		choicetable.setStyle(TableStyle([('ALIGN',(0,0),(-1,-1),'LEFT'),
+								('VALIGN',(0,0),(-1,-1),'TOP'),
+								('SPAN',(0,0),(1,0)),
+								]))
+		extratable=Table(extradata)
+		extratable.setStyle(TableStyle([('ALIGN',(0,0),(-1,-1),'LEFT'),
+								('VALIGN',(0,0),(-1,-1),'TOP'),
+								]))
+		
+		choicetable.setStyle(TableStyle([('ALIGN',(0,0),(-1,-1),'LEFT'),
+								('VALIGN',(0,0),(-1,-1),'TOP'),
+								('SPAN',(0,0),(1,0)),
+								]))		
+		payinotable=Table(payinfodata)
+		appdectable=Table(appdecdata)		
+		appdectable.setStyle(TableStyle([('ALIGN',(0,0),(-1,-1),'LEFT'),
+								('VALIGN',(0,0),(-1,-1),'TOP'),
+								('SPAN',(0,0),(1,0)),
+								]))				
+		
+		parentdectable=Table(parentdecdata)		
+		parentdectable.setStyle(TableStyle([('ALIGN',(0,0),(-1,-1),'LEFT'),
+								('VALIGN',(0,0),(-1,-1),'TOP'),
+								('SPAN',(0,0),(1,0)),
+								]))				
+		officeusetable=Table([[officeusetext,barcode]],rowHeights=[70*mm])
+		officeusetable.setStyle(TableStyle([('ALIGN',(0,0),(-1,-1),'CENTER'),
+								('VALIGN',(0,0),(-1,-1),'CENTER'),
+								('OUTLINE',(0,0),(-1,-1),.2*mm,colors.black),
+								]))		
+
+		basicinfotable=Table([[nametext,name],[resphonetext,resphone],[mobphonetext,mobphone],[gendertext,gender]])
+		basicinfotable.setStyle(TableStyle([('ALIGN',(0,0),(-1,-1),'LEFT'),
+								('VALIGN',(0,0),(-1,-1),'TOP'),
+								]))				
+	
+
+		metatable=Table([[Category],[basicinfotable]])
+		metatable.setStyle(TableStyle([('ALIGN',(0,0),(-1,-1),'CENTER'),
+								('VALIGN',(0,0),(-1,-1),'TOP'),
+								]))
+		infotable=Table([[metatable,photo]])
+		infotable.setStyle(TableStyle([('ALIGN',(0,0),(-1,-1),'CENTER'),
+								('VALIGN',(0,0),(-1,-1),'TOP'),
+								('OUTLINE',(0,0),(-1,-1),.2*mm,colors.black),
+								]))
+		
+		App=[]
+		App.append(headerinstable)
+		App.append(Spacer(3, 12))
+		App.append(titletable)
+		App.append(Spacer(1, 12))
+		App.append(infotable)
+		App.append(Spacer(3, 12))
+		App.append(personifotable)
+		App.append(linetable)
+		App.append(eexamtable)
+		App.append(linetable)
+		App.append(qexamtable)
+		App.append(linetable)
+		App.append(choicetable)
+		App.append(linetable)
+		App.append(extratable)
+		App.append(linetable)
+		App.append(payinotable)
+		App.append(linetable)
+		App.append(appdectable)
+		App.append(linetable)
+		App.append(parentdectable)
+		App.append(linetable)
+		App.append(officeusetable )
+		App.append(headerinstable)
+		insbreak=CondPageBreak(700)
+		App.append(insbreak)
+
+		App.append(headercantable)
+		App.append(Spacer(3, 12))
+		App.append(titletable)
+		App.append(Spacer(1, 12))
+		App.append(infotable)
+		App.append(Spacer(3, 12))
+		App.append(personifotable)
+		App.append(linetable)
+		App.append(eexamtable)
+		App.append(linetable)
+		App.append(qexamtable)
+		App.append(linetable)
+		App.append(choicetable)
+		#App.append(linetable)
+		App.append(extratable)
+		App.append(linetable)
+		App.append(payinotable)
+		App.append(linetable)
+		App.append(appdectable)
+		App.append(linetable)
+		App.append(parentdectable)
+		App.append(linetable)
+		#App.append(officeusetable )
+		App.append(headercantable)
+		doc.build(App)
 		
 
 application = webapp.WSGIApplication(
